af1bb82bd586f87775cb3a250fd0d6a99a96dd8b
Trevor Bai <baijin2011@gmail.com>
2020-04-14 00:58:12 -0400

    add a sorter class for array

--- build/index.js
+++ build/index.js
@@ -1,2 +1,34 @@
 "use strict";
-console.log('hi there');
+var Sorter = /** @class */ (function () {
+    // collection: number[];
+    // constructor(collection: number[]) {
+    //   this.collection = collection;
+    // }
+    // Equivalent to above
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        var noSwaps;
+        var swap = function (arr, idx1, idx2) {
+            var _a;
+            _a = [arr[idx2], arr[idx1]], arr[idx1] = _a[0], arr[idx2] = _a[1];
+        };
+        for (var i = length; i > 0; i--) {
+            noSwaps = true;
+            for (var j = 0; j < i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    swap(this.collection, j, j + 1);
+                    noSwaps = false;
+                }
+            }
+            if (noSwaps)
+                break;
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log('hi there');
+class Sorter {
+  // collection: number[];
+  // constructor(collection: number[]) {
+  //   this.collection = collection;
+  // }
+
+  // Equivalent to above
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+    let noSwaps: boolean;
+
+    const swap = (arr: number[], idx1: number, idx2: number) => {
+      [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    }
+
+    for (let i = length; i > 0; i--) {
+      noSwaps = true;
+      for (let j = 0; j < i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          swap(this.collection, j, j + 1);
+          noSwaps = false;
+        }
+      }
+      if (noSwaps) break;
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
